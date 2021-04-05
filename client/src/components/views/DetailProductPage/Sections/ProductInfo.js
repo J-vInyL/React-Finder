f7fd728b98c291f9ee513b529042a4bda6fc9048
J-vInyL <wnstjq7894@gmail.com>
2021-04-05 20:48:37 +0900

    카트 페이지 구현 및 물품 카운트 기능 구현

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Descriptions, Button } from "antd";
-
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../../_actions/user_actions";
 function ProductInfo(props) {
+  const dispatch = useDispatch();
+
+  const clickHandler = () => {
+    //필요한 정보를 cart 필드에다가 넣어준다 상품에 대한 아이디 개수
+    dispatch(addToCart(props.detail._id));
+  };
+
   return (
     <div>
       <Descriptions title={props.detail.title}>
@@ -19,7 +27,7 @@ function ProductInfo(props) {
       <br />
       <br />
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <Button size="large" shape="round" type="danger" onClick>
+        <Button size="large" shape="round" type="danger" onClick={clickHandler}>
           장바구니
         </Button>
       </div>
